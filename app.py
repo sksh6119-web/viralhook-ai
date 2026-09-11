16c9a08d7825bb66266405e35b521fd2a77836e4
sksh6119-web <sksh6119@gmail.com>
2026-09-11 21:31:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ if st.button("Generate Viral Pack"):
                 Topic: {topic}
                 """
                 completion = client.chat.completions.create(
-                    model="llama3-70b-8192",
+                    model="llama-3.1-8b-instant",
+
                     messages=[{"role": "user", "content": prompt}],
                     temperature=0.7,
                 )
